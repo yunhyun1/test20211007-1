ec055d10fb5bbf5c850cb86b5ea016c161ad54fb
yunhyun <cyh0107@gmail.com>
2021-10-01 17:23:54 +0900

    'hello3'

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
-		System.out.println("hello2");
+		System.out.println("hello2"); 
+		System.out.println("hello3");
 	}
 
 }
